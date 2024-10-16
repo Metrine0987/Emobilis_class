37e6a25dba0db01f502d3da830aaf9518181785b
Metrine0987 <metrine.regina@gmail.com>
2024-10-16 23:35:17 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const colorButton = document.getElementById("colorButton")
+colorButton.addEventListener("click" , ()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor==='blue' ? '#4caf50':'blue'
+})
+
+
+function myFunction() {
+    document.getElementById("paragraphClick").innerHTML = "Button has already been clicked.";
+  }
